5da86c4322ff84e1d0b45b0f69f8199bddc707f3
christianhederson22@gmail.com <christianhederson22@gmail.com>
2024-06-28 17:30:07 +0000

    Adição dos scripts do databricks ao repositório

---
+++ scripts/Gerar dados.py
@@ -0,0 +1,346 @@
+# Databricks notebook source
+# MAGIC %pip install faker
+# MAGIC %pip install pyodbc
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+# MAGIC curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+# MAGIC
+# MAGIC sudo apt-get update
+# MAGIC sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# MAGIC # optional: for bcp and sqlcmd
+# MAGIC sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+# MAGIC echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+# MAGIC source ~/.bashrc
+
+# COMMAND ----------
+
+import pyodbc
+from faker import Faker
+import random
+from datetime import datetime
+
+# Configurações do SQL Server
+server = 'petit-gato.database.windows.net'
+schema = 'dbo'
+username = 'petit-gato'
+password = 'c4puRRc!n0'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+connection_string = f'DRIVER={driver};SERVER={server};DATABASE=master;UID={username};PWD={password}'
+try:
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute("""
+    CREATE DATABASE petitgato;
+    """)
+    conn.commit()
+except pyodbc.Error as ex:
+    print(f"Erro ao conectar ao SQL Server: {ex}")
+finally:
+    try:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    except pyodbc.Error as ex:
+        print(f"Erro ao fechar a conexão: {ex}")
+
+connection_string = f'DRIVER={driver};SERVER={server};DATABASE=petitgato;UID={username};PWD={password}'
+
+# Lista de nomes e categorias temáticos
+items = [
+    "Catfé", "Catppuccino", "Gato pingado", "Mia não latte", "Affogato",
+    "Miaukshake de morango", "Miaukshake de chocolate", "Gato quente",
+    "Gatinho caramelado", "Miau-Chocolat", "Gatolate quente", "Felincha de limão",
+    "Gateado mousse", "Miaunara", "Whiskerado", "Chafé com leite", "Gato frappé",
+    "Meowcha verde", "Cookie de bigodes", "Miauccino de avelã", "Latte Gatinho",
+    "FrapaGato Caramelado", "Miaulicano", "Gatoccino Romeu e Julieta",
+    "Miaramelo Macchiato", "Gatolatte de Amêndoas", "Miaroma", "Miauccino Mentolado",
+    "Capurrcino", "Megro gato", "Miaukie Cream", "Catlix de baunilha", 
+    "Frapurrcino de Caramelo", "Cappurrcino", "Gateado de leite", "Gato-ffee",
+    "Miarinha Latte", "Café com Gato", "Miaudorinha", "Latte Gatunho",
+    "Meowcat Macchiato", "Miau Espresso", "Mocha Rafinhado", "Gatone de Caramelo",
+    "Mooga Mocha", "Latte Gatúnico", "Café Meowmaid", "Chá Miaumato",
+    "Frapurrcino de Morango", "Chafé Gelado", "Miaucalate de Menta", "Cafelinado",
+    "Leite Gatado", "Miaudinho de Canela", "Latte de Noz-Moscada", "Capufelino",
+    "Miarão Gelado", "Gato de Vinho", "Gato Affogato", "Latte Maçaricatto",
+    "Catópolis", "Latte de Marshmallow", "Capurrcino de Limão", "Esfregatasso",
+    "Café Bola de Pêlo", "Capurrcino de Mel", "Gatoscatto", "Latte Gatosão",
+    "Felinário Frapuccino", "Café Rascunho de Gato", "Latte Framboesa",
+    "Catlix de Chocolate", "Gato Café com Chantilly", "Miau de Cereja",
+    "Meowssissippi Mudslide", "Cafelatte com Gatomix", "Café de Gatomel",
+    "Latte Miauninho", "Miauwauki Gelado", "Frangelho de Gato", "Latte de Gato-Canela",
+    "Gatoscuro Gelado", "Miaudrinho", "Macciagato", "Frapúrrino de Gengibre",
+    "Latte Miaucado", "Gatoccino Melancia", "Café com Miaucha", "Felina Capurrcino",
+    "Latte Bichano", "Miarte Latte", "Carumiau Capurrcino", "Gatomallow Frapuccino",
+    "Moostachio Mocha", "Vanilla Catccino", "Felinato Frapuccino", "Capurrmón de caneca",
+    "Chá Miauton", "Macallade de Frapuccino", "Café Miadês", "Lasgato Latte",
+    "Gatia Mocha", "Miaulino Espresso", "Gatto Gelato", "Cafairo de Gato",
+    "Latte Gateado", "Gato Bolhudo", "Miauto Milkshake", "Purrcellino Cappuccino",
+    "Catinsel Frappuccino", "Catish Capurrccino", "Matcha Meowcha", "Frapegatto Gelado",
+    "Miaillante Cappuccino", "Gatificante Mocha", "Latte Miaudiente",
+    "Frapurrcino Pistache", "Caragato Latte", "Café Gattino", "Latte Nebulinha",
+    "Gato Latte Caramelo", "Miaucat Gelado", "Latte Miaucake", "Gatocciato",
+    "Miaurvado Espresso", "Gatola Mocha", "Café no Pêlo", "Latte Caturégico",
+    "Meowmango Smoothie", "Felino Quente", "Café Miagalho", "Latte Gatômico",
+    "Café Miauchocolatado", "Latte Gattoso", "Capurrcino de Hortelã",
+    "Meowmy Sake", "Gatomousse", "Latte Miaucupido", "Capurrmón Gelado",
+    "Meowjito", "Latte Gatudissimo", "Miarucino", "Gattie Milkshake de Amora",
+    "Gato Tigrado", "Café Miaucremoso", "Miaudicínio", "Felizante Latte",
+    "Capurrcino de Pessego", "Gato Milk Latte", "Miarino Mocha", "Catadoçura",
+    "Café Quato", "Latte Beirmiau", "Miaulizado Espresso", "Latte Miautor",
+    "Gatomelo Frapuccino", "Latte Miaulato", "Miaulina Gelada", "Frapurrcino de Melão",
+    "Latte Miauberry", "Cataculino", "Capurrcino Espesso", "Gauffe de Gato"
+]
+
+categories = [
+    "Café", "Café", "Café", "Café", "Café",
+    "MilkShake", "MilkShake", "Bebida Quente", "Sobremesa", "Chocolate Quente",
+    "Bebida Quente", "Sobremesa", "Sobremesa", "Bebida Gelada", "Bebida Alcoólica",
+    "Bebida Quente", "Bebida Gelada", "Bebida Quente", "Sobremesa", "Bebida Quente",
+    "Café", "Bebida Gelada", "Café", "Café", "Café", "Café", "Café", "Bebida Quente",
+    "Café", "MilkShake", "Sobremesa", "Bebida Gelada", "Café", "Café", "Bebida Quente",
+    "Café", "Bebida Quente", "Bebida Quente", "Bebida Alcoólica", "Bebida Quente",
+    "Café", "Café", "Café", "Café", "Café", "Café", "Café", "Bebida Quente", 
+    "Bebida Gelada", "Bebida Quente", "Bebida Quente", "Café", "Sobremesa", 
+    "Bebida Quente", "Bebida Gelada", "Bebida Alcoólica", "Bebida Alcoólica", 
+    "Café", "Café", "Café", "Bebida Quente", "Café", "Café", "Café", "Café",
+    "Bebida Gelada", "Café", "Bebida Quente", "Café", "Café", "Café", "Café", 
+    "Bebida Quente", "Bebida Quente", "Café", "Bebida Quente", "Bebida Quente",
+    "Café", "Bebida Gelada", "Café", "Café", "Bebida Gelada", "Bebida Alcoólica",
+    "Bebida Quente", "Café", "Bebida Quente", "Bebida Gelada", "Café", "Bebida Gelada",
+    "Café", "Bebida Gelada", "Bebida Quente", "Café", "Bebida Quente", "Bebida Gelada",
+    "Bebida Quente", "Café", "Bebida Alcoólica", "Café", "Café", "Café", "Café",
+    "MilkShake", "Bebida Quente", "Bebida Gelada", "Café", "Bebida Gelada",
+    "Bebida Quente", "Café", "Café", "Bebida Quente", "Café", "Sobremesa",
+    "Café", "Bebida Quente", "Café", "Bebida Gelada", "Bebida Gelada", "Café",
+    "Bebida Gelada", "Café", "Bebida Quente", "Bebida Alcoólica", "Café", "Café",
+    "Café", "Bebida Gelada", "Bebida Quente", "Café", "Bebida Quente", "Sobremesa",
+    "Bebida Quente", "Bebida Gelada", "Café", "Café", "Sobremesa", "Sobremesa",
+    "Café", "MilkShake", "Café", "Café", "Sobremesa", "Bebida Gelada", "Café",
+    "Café", "Café", "Café", "Bebida Gelada", "Café", "Café", "Bebida Quente",
+    "Sobremesa", "Café", "Bebida Quente", "Bebida Gelada", "Café", "Café",
+    "Café", "Café", "Café", "MilkShake", "Bebida Gelada", "Bebida Quente"
+]
+
+try:
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Criar tabelas
+
+    cursor.execute("""
+    IF OBJECT_ID('cat_customer', 'U') IS NOT NULL DROP TABLE dbo.cat_customer;
+    IF OBJECT_ID('payment', 'U') IS NOT NULL DROP TABLE dbo.payment;
+    IF OBJECT_ID('orderitem', 'U') IS NOT NULL DROP TABLE dbo.orderitem;
+    IF OBJECT_ID('customerorder', 'U') IS NOT NULL DROP TABLE dbo.customerorder;
+    IF OBJECT_ID('cat', 'U') IS NOT NULL DROP TABLE dbo.cat;
+    IF OBJECT_ID('item', 'U') IS NOT NULL DROP TABLE dbo.item;
+    IF OBJECT_ID('coffeetable', 'U') IS NOT NULL DROP TABLE dbo.coffeetable;
+    IF OBJECT_ID('customer', 'U') IS NOT NULL DROP TABLE dbo.customer;
+    IF OBJECT_ID('category', 'U') IS NOT NULL DROP TABLE dbo.category;
+
+  
+   
+    CREATE TABLE dbo.category (
+      categoryid INT IDENTITY(1,1) PRIMARY KEY,
+      description VARCHAR(100) NOT NULL
+    );
+
+    CREATE TABLE dbo.item (
+      itemid INT IDENTITY(1,1) PRIMARY KEY,
+      categoryid INT NOT NULL,
+      description VARCHAR(100) NOT NULL,
+      active BIT NOT NULL DEFAULT 1,
+      price DECIMAL(15,2) NOT NULL,
+      cost DECIMAL(15,2) NOT NULL,
+      CONSTRAINT fk_item__category FOREIGN KEY (categoryid) REFERENCES category (categoryid)
+    );
+
+    CREATE TABLE dbo.coffeetable (
+      tableid INT IDENTITY(1,1) PRIMARY KEY,
+      occupied BIT NOT NULL DEFAULT 0
+    );
+
+    CREATE TABLE dbo.customer (
+      customerid INT IDENTITY(1,1) PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      phone VARCHAR(30),
+      email VARCHAR(100),
+      cpf VARCHAR(11)
+    );
+
+    CREATE TABLE dbo.customerorder (
+      orderid INT IDENTITY(1,1) PRIMARY KEY,
+      customerid INT NOT NULL,
+      tableid INT NOT NULL,
+      date DATETIME NOT NULL,
+      opened BIT NOT NULL DEFAULT 0,
+      CONSTRAINT fk_customerorder__table FOREIGN KEY (tableid) REFERENCES coffeetable (tableid),
+      CONSTRAINT fk_customerorder__customer FOREIGN KEY (customerid) REFERENCES customer (customerid)
+    );
+
+    CREATE TABLE dbo.orderitem (
+      orderitemid INT IDENTITY(1,1) PRIMARY KEY,
+      orderid INT NOT NULL,
+      itemid INT NOT NULL,
+      quantity INT NOT NULL,
+      CONSTRAINT fk_orderitem__customerorder FOREIGN KEY (orderid) REFERENCES customerorder (orderid),
+      CONSTRAINT fk_orderitem__item FOREIGN KEY (itemid) REFERENCES item (itemid)
+    );
+
+    CREATE TABLE dbo.payment (
+      paymentid INT IDENTITY(1,1) PRIMARY KEY,
+      customerid INT NOT NULL,
+      orderid INT NOT NULL,
+      value DECIMAL(15,2) NOT NULL,
+      date DATETIME NOT NULL,
+      CONSTRAINT fk_payment__customer FOREIGN KEY (customerid) REFERENCES customer (customerid),
+      CONSTRAINT fk_payment__customerorder FOREIGN KEY (orderid) REFERENCES customerorder (orderid)
+    );
+
+    CREATE TABLE dbo.cat (
+      catid INT IDENTITY(1,1) PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      age INT NOT NULL,
+      adoption_date DATETIME NOT NULL
+    );
+
+    CREATE TABLE dbo.cat_customer (
+      cat_customerid INT IDENTITY(1,1) PRIMARY KEY,
+      catid INT NOT NULL,
+      customerid INT NOT NULL,
+      adoption_date DATETIME NOT NULL,
+      CONSTRAINT fk_cat_customer__cat FOREIGN KEY (catid) REFERENCES cat (catid),
+      CONSTRAINT fk_cat_customer__customer FOREIGN KEY (customerid) REFERENCES customer (customerid)
+    );
+
+ 
+    """)
+    conn.commit()
+    # Gerar dados falsos
+    fake = Faker()
+
+    # Inserir dados nas tabelas independentes
+    category_id_map = {}
+
+    for category in set(categories):
+        cursor.execute('''
+            INSERT INTO category (description) OUTPUT INSERTED.categoryid VALUES (?)
+        ''', category)
+        category_id = cursor.fetchone()[0]
+        category_id_map[category] = category_id
+    conn.commit()
+
+    # Inserir itens na tabela item com referência ao ID da categoria
+    for description, category in zip(items, categories):
+        category_id = category_id_map[category]
+        active = fake.boolean()
+        price = fake.pydecimal(left_digits=2, right_digits=2, positive=True, min_value=2)
+        cost = fake.pydecimal(left_digits=2, right_digits=2, positive=True, max_value=price)
+        cursor.execute("INSERT INTO item (categoryid, description, active, price, cost) VALUES (?, ?, ?, ?, ?)", (category_id, description, active, price, cost))
+    conn.commit()
+
+    for _ in range(30):
+        # Inserir mesa de café
+        occupied = fake.boolean()
+        cursor.execute("INSERT INTO coffeetable (occupied) VALUES (?)", (occupied,))
+    conn.commit()
+
+    for _ in range(5000):
+        # Inserir cliente
+        name = fake.name()
+        phone = fake.phone_number()
+        email = fake.email()
+        cpf = fake.bothify(text='###########')
+        cursor.execute("INSERT INTO customer (name, phone, email, cpf) VALUES (?, ?, ?, ?)", (name, phone, email, cpf))
+    conn.commit()
+
+    # Inserir gatos
+    for _ in range(2000):
+        cat_name = fake.first_name()
+        age = random.randint(1, 15)
+        adoption_date = fake.date_time_between(start_date='-3y', end_date='-2y')
+        cursor.execute("INSERT INTO cat (name, age, adoption_date) VALUES (?, ?, ?)", (cat_name, age, adoption_date))
+    conn.commit()
+
+    customer_ids = [i for i in range(1, 5001)]
+    table_ids = [i for i in range(1, 31)]
+    item_ids = [i for i in range(1, items.__len__())]
+    cat_ids = [i for i in range(1, 2001)]
+
+    # Inserir dados nas tabelas dependentes
+    existing_order_ids = []
+
+    for _ in range(10000):
+        # Inserir pedido do cliente
+        customer_id = random.choice(customer_ids)
+        table_id = random.choice(table_ids)
+
+        # Verificar se customer_id e table_id existem nas tabelas customer e coffeetable
+        cursor.execute("SELECT 1 FROM customer WHERE customerid = ?", (customer_id,))
+        if not cursor.fetchone():
+            raise ValueError(f"customer_id {customer_id} não encontrado na tabela 'customer'")
+
+        cursor.execute("SELECT 1 FROM coffeetable WHERE tableid = ?", (table_id,))
+        if not cursor.fetchone():
+            raise ValueError(f"table_id {table_id} não encontrado na tabela 'coffeetable'")
+
+        date = fake.date_time_between(start_date='-3y', end_date=datetime.now())
+        opened = fake.boolean()
+        cursor.execute("INSERT INTO customerorder (customerid, tableid, date, opened) VALUES (?, ?, ?, ?)", (customer_id, table_id, date, opened))
+        order_id = _ + 1
+        cursor.execute("SELECT 1 FROM customerorder WHERE orderid = ?", (order_id,))
+
+        if not cursor.fetchone():
+            raise ValueError("Falha ao inserir na tabela 'customerorder'")
+
+        existing_order_ids.append(order_id)
+
+        # Inserir item do pedido
+        for __ in range (random.randint(1,10)):
+          item_id = random.choice(item_ids)
+          quantity = random.randint(1, 10)
+          cursor.execute("INSERT INTO orderitem (orderid, itemid, quantity) VALUES (?, ?, ?)", (order_id, item_id, quantity))
+        
+        if (_ % 1000 == 0):
+            conn.commit()
+    conn.commit()
+
+    # Calcular o total do pedido e inserir pagamentos
+    for order_id in existing_order_ids:
+        cursor.execute("SELECT customerid, date FROM customerorder WHERE orderid = ?", (order_id,))
+        customer_id, date = cursor.fetchone()
+
+        cursor.execute("SELECT SUM(quantity * price) FROM orderitem JOIN item ON orderitem.itemid = item.itemid WHERE orderid = ?", (order_id,))
+        total_items = cursor.fetchone()[0] or 0
+    
+        cursor.execute("INSERT INTO payment (customerid, orderid, value, date) VALUES (?, ?, ?, ?)", (customer_id, order_id, total_items, date))
+    conn.commit()
+
+    # Inserir adoções de gatos por clientes
+    for _ in range(2000):
+        cat_id = cat_ids[_]
+        customer_id = random.choice(customer_ids)
+        adoption_date = fake.date_time_between(start_date='-2y', end_date=datetime.now())
+        cursor.execute("INSERT INTO cat_customer (catid, customerid, adoption_date) VALUES (?, ?, ?)", (cat_id, customer_id, adoption_date))
+    conn.commit()
+
+    print("Dados gerados e inseridos com sucesso!")
+
+except pyodbc.Error as ex:
+    print(f"Erro ao conectar ao SQL Server: {ex}")
+
+finally:
+    try:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    except pyodbc.Error as ex:
+        print(f"Erro ao fechar a conexão: {ex}")
+
---
+++ scripts/PetitGato - Bronze.py
@@ -0,0 +1,145 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Validando a SparkSession
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Conectando Azure ADLS Gen2 no Databricks
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Mostrando os pontos de montagem no cluster Databricks
+
+# COMMAND ----------
+
+display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Definindo uma função para montar um ADLS com um ponto de montagem com ADLS SAS 
+
+# COMMAND ----------
+
+storageAccountName = "datalake271297fb88ef6733"
+sasToken = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2024-06-29T00:29:26Z&st=2024-06-28T16:29:26Z&spr=https&sig=NPxuYKGVwZPiye%2FpKprMavdDoJLyIFt5WfkJE40TNFE%3D"
+
+def mount_adls(blobContainerName):
+    try:
+      dbutils.fs.mount(
+        source = "wasbs://{}@{}.blob.core.windows.net".format(blobContainerName, storageAccountName),
+        mount_point = f"/mnt/{storageAccountName}/{blobContainerName}",
+        extra_configs = {'fs.azure.sas.' + blobContainerName + '.' + storageAccountName + '.blob.core.windows.net': sasToken}
+      )
+      print("OK!")
+    except Exception as e:
+      print("Falha", e)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Montando todos os containers
+
+# COMMAND ----------
+
+mount_adls('landing-zone')
+mount_adls('bronze')
+mount_adls('silver')
+mount_adls('gold')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Mostrando os pontos de montagem no cluster Databricks
+
+# COMMAND ----------
+
+display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Mostrando todos os arquivos da camada landing-zone
+
+# COMMAND ----------
+
+display(dbutils.fs.ls(f"/mnt/{storageAccountName}/landing-zone/petitgato/"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Gerando um dataframe para cada arquivo a partir dos arquivos CSV gravado no container landing-zone do Azure Data Lake Storage
+
+# COMMAND ----------
+
+df_customerorder = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/customerorder.csv")
+df_orderitem     = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/orderitem.csv")
+df_item          = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/item.csv")
+df_customer      = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/customer.csv")
+df_category      = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/category.csv")
+df_coffeetable   = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/coffeetable.csv")
+df_payment       = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/payment.csv")
+df_cat           = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/cat.csv")
+df_customercat   = spark.read.option("infeschema", "true").option("header", "true").csv(f"/mnt/{storageAccountName}/landing-zone/petitgato/cat_customer.csv")
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Adicionando metadados de data e hora de processamento e nome do arquivo de origem
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, lit
+
+df_customerorder = df_customerorder.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("customerorder.csv"))
+df_orderitem     = df_orderitem.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("orderitem.csv"))
+df_item          = df_item.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("item.csv"))
+df_customer      = df_customer.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("customer.csv"))
+df_category      = df_category.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("category.csv"))
+df_coffeetable   = df_coffeetable.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("coffeetable.csv"))
+df_payment       = df_payment.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("payment.csv"))
+df_cat           = df_cat.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("cat.csv"))
+df_customercat   = df_customercat.withColumn("data_hora_bronze", current_timestamp()).withColumn("nome_arquivo", lit("cat_customer.csv"))
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Salvando os dataframes em delta lake (formato de arquivo) no data lake (repositorio cloud)
+
+# COMMAND ----------
+
+df_customerorder.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/customerorder")
+df_orderitem.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/orderitem")
+df_item.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/item")
+df_customer.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/customer")
+df_category.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/category")
+df_coffeetable.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/coffeetable")
+df_payment.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/payment")
+df_cat.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/cat")
+df_customercat.write.format('delta').save(f"/mnt/{storageAccountName}/bronze/customercat")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Verificando os dados gravados em delta na camada bronze
+
+# COMMAND ----------
+
+display(dbutils.fs.ls(f"/mnt/{storageAccountName}/bronze/"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Lendo um exemplo de um delta lake para validar a existencia dos dados e das colunas do metadados
+
+# COMMAND ----------
+
+spark.read.format('delta').load(f'/mnt/{storageAccountName}/bronze/cat').limit(10).display()
